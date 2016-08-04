40c89bb2f987bb163f4138e617d8069edeed76ac
pedropedro <pedrohojas@gmail.com>
2016-08-04 14:51:56 -0400

    Agregué el index y los elementos necesarios

---
+++ elementos/navigation.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-inverse">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Git es bacán!</a>
+    </div>
+    <div id="navbar" class="collapse navbar-collapse">
+      <ul class="nav navbar-nav">
+        <li><a href="paginas/Pedro"> Pedro</a></li>
+        <li><a href="#">Freddy</a></li>
+        <li><a href="#">Mandak</a></li>
+        <li><a href="#">Aron</a></li>
+        <li><a href="#">Francisco</a></li>
+        <li><a href="#">Richi</a></li>
+        <li><a href="#">Alex</a></li>
+        <li><a href="#">Sergio</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
