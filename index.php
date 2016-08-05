edbdf4f5395b49d64c0466b43216a4fe3bb9e6a8
Michel Mandakovic <mmandakovic@dimerc.cl>
2016-08-05 10:45:15 -0300

    He commiteado algo en la pagina #7

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <div class="starter-template">
       <h1>Nuestra página es bacán</h1>
       <p class="lead">Este es tutorial de Freddy y Pedro.<br>¡Es bacán!.</p>
+      <p class="lead">ACA PASO EL MICHEL</p>
     </div>
   </div>
 </body>
